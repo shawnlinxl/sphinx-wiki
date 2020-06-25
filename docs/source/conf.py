6483713499ccaa8d7fc7be0da9393864467d6189
Shawn Lin <xiaoliang.lin@columbia.edu>
2020-06-25 04:22:21 +0000

    Add favicon

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -47,6 +47,7 @@ exclude_patterns = []
 
 html_theme = "pydata_sphinx_theme"
 html_logo = "_static/logo.png"
+html_favicon = "_static/favicon.ico"
 
 html_theme_options = {
     "github_url": "https://github.com/shawnlinxl/sphinx-wiki",
